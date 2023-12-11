cfe36f1450a5806c7c23555898a03093bb1e65c5
Thomas Gasangwa <thomasgasangwa@gmail.com>
2023-12-11 15:33:23 +0200

    feat: create the category files for handling routes, business logic and connecting to the db

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TaskModule } from './task/task.module';
+import { CategoryController } from './category/category.controller';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  controllers: [AppController],
+  controllers: [AppController, CategoryController],
   providers: [AppService],
-  imports: [TaskModule],
+  imports: [TaskModule, CategoryModule],
 })
 export class AppModule {}

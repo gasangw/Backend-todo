199ba6ab156181744515b56a953e7a413b5a6a39
Thomas Gasangwa <thomasgasangwa@gmail.com>
2023-12-11 14:54:20 +0200

    feat: create the initial todos files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TodosController } from './todos/todos.controller';
+import { TodosModule } from './todos/todos.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [TodosModule],
+  controllers: [AppController, TodosController],
   providers: [AppService],
 })
 export class AppModule {}

d34f05b7c2ac71a39ced891c9317acfb0ce817fd
Thomas Gasangwa <thomasgasangwa@gmail.com>
2023-12-12 12:57:05 +0200

    refactor: add swapper api property to category dtos

--- src/category/dto/category-dto.ts
+++ src/category/dto/category-dto.ts
@@ -1,6 +1,11 @@
 import { IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger/dist/decorators';
 
 export class CategoryDto {
+  @ApiProperty({
+    example: 'NestJS Framework',
+    required: true,
+  })
   @IsString()
   name: string;
 }

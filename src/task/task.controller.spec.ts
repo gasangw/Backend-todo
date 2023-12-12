774bbcf674addadc1fb12864518a225faf5a3879
Thomas Gasangwa <thomasgasangwa@gmail.com>
2023-12-12 16:42:57 +0200

    refactor: create test for task.controller

--- src/task/task.controller.spec.ts
+++ src/task/task.controller.spec.ts
@@ -1,12 +1,57 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TaskController } from './task.controller';
+import { TaskService } from './task.service';
+import { CategoryService } from '../category/category.service';
+import { Task } from '../task/entity/task.entity';
 
 describe('TaskController', () => {
   let controller: TaskController;
+  let fakeTaskService: Partial<TaskService>;
+  let fakeCategoryService: Partial<CategoryService>;
 
   beforeEach(async () => {
+    fakeTaskService = {
+      getAll: () => {
+        return Promise.resolve([
+          {
+            id: '1',
+            title: 'test',
+            description: 'testing in action',
+            status: 'OPEN',
+            categoryId: 'category1',
+          },
+        ] as Task[]);
+      },
+      getOne: (id: string) => {
+        return Promise.resolve({
+          id,
+          title: 'test',
+          description: 'testing in action',
+          status: 'OPEN',
+          categoryId: 'category1',
+        } as Task);
+      },
+      // create: () => {},
+      // delete: (id: string) => {
+      //   return Promise.resolve({ id });
+      // },
+    };
+    fakeCategoryService = {
+      // create: () => {},
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TaskController],
+      providers: [
+        {
+          provide: TaskService,
+          useValue: fakeTaskService,
+        },
+        {
+          provide: CategoryService,
+          useValue: fakeCategoryService,
+        },
+      ],
     }).compile();
 
     controller = module.get<TaskController>(TaskController);

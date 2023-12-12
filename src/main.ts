a3dff987351097104ccf92058617a7fae525d4ca
Thomas Gasangwa <thomasgasangwa@gmail.com>
2023-12-12 12:38:39 +0200

    refactor: add swagger to the app

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+    .setTitle('Nestjs Todo API')
+    .setDescription(
+      'This will enable you to create, read, and delete tasks as well as asign categories to them',
+    )
+    .setVersion('1.0')
+    .addServer('http://localhost:3000', 'Local server')
+    .addTag('tasks')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('Nestjs Todo API', app, document);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -11,6 +24,6 @@ async function bootstrap() {
       transform: true, // automatically transform the incoming data to DTO
     }),
   );
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();

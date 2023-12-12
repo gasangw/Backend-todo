8caf4107183bb317dcb69bf9dff78f07fe44fecf
Thomas Gasangwa <thomasgasangwa@gmail.com>
2023-12-12 19:40:53 +0200

    feat: create tests for category service

--- src/category/category.service.spec.ts
+++ src/category/category.service.spec.ts
@@ -1,18 +1,62 @@
-// import { Test, TestingModule } from '@nestjs/testing';
-// import { CategoryService } from './category.service';
+import { Test, TestingModule } from '@nestjs/testing';
+import { CategoryService } from './category.service';
+import { CategoryRepository } from './category.repository';
+import { Category } from './entity/category.entity';
 
-// describe('CategoryService', () => {
-//   let service: CategoryService;
+describe('CategoryService', () => {
+  let service: CategoryService;
+  let fakeCategoryRepository: Partial<CategoryRepository>;
 
-//   beforeEach(async () => {
-//     const module: TestingModule = await Test.createTestingModule({
-//       providers: [CategoryService],
-//     }).compile();
+  beforeEach(async () => {
+    fakeCategoryRepository = {
+      getAllCategories: () => {
+        return Promise.resolve([
+          {
+            id: 'category1',
+            name: 'test',
+          },
+        ]);
+      },
 
-//     service = module.get<CategoryService>(CategoryService);
-//   });
+      createCategory: () => {
+        return Promise.resolve();
+      },
 
-//   it('should be defined', () => {
-//     expect(service).toBeDefined();
-//   });
-// });
+      removeCategory: () => {
+        return Promise.resolve();
+      },
+    };
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        CategoryService,
+        {
+          provide: CategoryRepository,
+          useValue: fakeCategoryRepository,
+        },
+      ],
+    }).compile();
+
+    service = module.get<CategoryService>(CategoryService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('should return an array of categories', async () => {
+    const result = await service.getAllCategories();
+    expect(result).toBeInstanceOf(Array);
+  });
+
+  it('should create a category', () => {
+    const category = service.createCategory({
+      name: 'test',
+    });
+    expect(category).toBeUndefined();
+  });
+
+  it('should remove a category', async () => {
+    const category = await service.removeCategory('1');
+    expect(category).toBeUndefined();
+  });
+});

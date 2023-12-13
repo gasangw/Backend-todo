b9c94d8b1e3ed99eecc4fcbc8e4354533a3bf0e6
Thomas Gasangwa <thomasgasangwa@gmail.com>
2023-12-13 09:55:47 +0200

    refactor: remove commented codes

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TaskModule } from './task/task.module';
-// import { Config, JsonDB } from 'node-json-db';
 import { CategoryModule } from './category/category.module';
 
 @Module({
